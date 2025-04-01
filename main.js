6c121f92d199d63ce8acab049f52bc4a7dd3ee3c
Mheyrie <omota4fidelity@gmail.com>
2025-04-01 05:44:40 +0100

    fixed bug on harmburger

--- main.js
+++ main.js
@@ -46,3 +46,26 @@ const closeNav = () => {
 };
 
 closeBtn.addEventListener("click", closeNav);
+
+//Courses details
+const coursesContainer = document.querySelector(".courses__container");
+
+const courses = Array.from({ length: 18 }, (_, index) => ({
+  title: `Responsive Social Media UI Design ${index + 1} `,
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates!`,
+  image: `./images/course${index + 1}.jpg`,
+}));
+
+courses.forEach((course) => {
+  const courseElement = document.createElement("article");
+  courseElement.classList.add("course");
+  courseElement.innerHTML = `
+  <div class="course__image">
+                    <img src="${course.image}" alt="">
+                </div>
+                <h4>${course.title}</h4>
+                <p>${course.description}"</p>
+                <a href="courses.html" class="btn btn-primary">Learn More</a>
+  `;
+  coursesContainer.appendChild(courseElement);
+});
